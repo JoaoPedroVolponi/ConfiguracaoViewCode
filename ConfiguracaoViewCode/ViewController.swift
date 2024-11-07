a2e8962405986c1f433eaa6c28014a0a4eeaf775
João Pedro Volponi <jpvrosada@hotmail.com>
2024-11-07 18:46:25 -0300

    Projeto criado

---
+++ ConfiguracaoViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConfiguracaoViewCode
+//
+//  Created by João Pedro Volponi on 07/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
